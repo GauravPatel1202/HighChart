da2c86d6b95a7fc8f97bc3189b6ff263e01f70ae
anekola <anekola@Mac-C02HM2HWDHJ.local>
2014-09-03 17:15:12 -0400

    Starting work on poll

---
+++ poll.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>Highcharts How-To - ONA14</title>
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="css/reveal.min.css">
+		<link rel="stylesheet" href="css/theme/default.css" id="theme">
+
+		<!-- For syntax highlighting -->
+		<link rel="stylesheet" href="lib/css/zenburn.css">
+
+		<style>
+		
+		</style>
+
+	</head>
+
+	<body>
+
+
+		<form>
+			<div>
+				<label>Have you ever used Highcharts?</label>
+				<select name="highcharts">
+					<option>Yes</option>
+					<option>No</option>
+				</select>
+			</div>
+			<div>
+				<label>What other tools have you used for data visualization?</label>
+				<input type="checkbox" name="dataviz" value="Google Charts">Google Charts<br />
+				<input type="checkbox" name="dataviz" value="D3.js">D3.js<br />
+				<input type="checkbox" name="dataviz" value="Raphael.js">Raphael.js<br />
+				<input type="checkbox" name="dataviz" value="Mapbox">Mapbox<br />
+				<input type="checkbox" name="dataviz" value="Leaflet">Leaflet<br />
+			</div>
+			<div>
+				<label>How well do you know Javascript/jQuery?</label>
+				<select name="javascript">
+					<option>0</option>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+				</select>
+			</div>
+			<div>
+				<label>What CMS does your organization use?</label>
+				<select name="cms">
+					<option>WordPress</option>
+					<option>Drupal</option>
+					<option>Joomla</option>
+					<option>Other</option>
+				</select>
+			</div>
+			<div>
+				<label>Are you going to the Highcharts session on Saturday?</label>
+				<select name="attendance">
+					<option>Yes</option>
+					<option>No</option>
+				</select>
+			</div>
+
+
+		</form>
+
+
+
+
+	</body>
+</html>
